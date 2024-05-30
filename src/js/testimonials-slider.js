c04fdc2a97579e2ddcb7b6c5807c7fa2470eb675
Olga Oliynik <poddubnaolga@gmail.com>
2024-05-30 20:44:21 +0300

    Revert "Fix performance. Fiz form patterns."
    This reverts commit 37bc61e296ace3e6467772e041d449ada7193df1.

--- src/js/testimonials-slider.js
+++ src/js/testimonials-slider.js
@@ -1,4 +1,6 @@
 // core version + navigation, pagination modules:
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
 // import Swiper and modules styles
 import 'swiper/css';
 import 'swiper/css/navigation';
@@ -22,9 +24,6 @@ const swiperParams = {
             slidesPerView: 3,
             spaceBetween: 32
         },
-    },
-    pagination: {
-        el: '.swiper-pagination',
     },
     on: {
         init() {
